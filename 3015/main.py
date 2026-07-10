d3ba5ebd2ca3f6cb695fa698b0093024ee07baa9
69070227 <69070227@kmitl.ac.th>
2026-07-10 12:40:21 +0700

    Update main.py

--- 3015/main.py
+++ 3015/main.py
@@ -6,9 +6,14 @@ y = 3
 a = int(input()) #สมมติราคาต่อคน
 z = int(input()) #จำนวนคน
 
-
 pro = 0
 
 if z >= x:
-    
+    pro = a*y
+    total = pro
+    print(total)
 
+else:
+    total = a*x
+    print(total)
+    
