58cd9cf41353fc750e3b43c5a6cc3cefb6347b5c
69070227 <69070227@kmitl.ac.th>
2026-07-14 21:42:33 +0700

    add

--- 3015/main.py
+++ 3015/main.py
@@ -1,11 +1,16 @@
 """Pro"""
+def main():
+    """"main"""
+    x = int(input())
+    y = int(input())
 
-x = int(input())
-y = int(input())
+    a = int(input()) #สมมติราคาต่อคน
+    z = int(input()) #จำนวนคน
 
-a = int(input()) #สมมติราคาต่อคน
-z = int(input()) #จำนวนคน
+    come = z//x
+    pay = z % x
 
-pro = (z//x)
+    total = (come * y + pay) * a
+    print(total)
 
-total = 0
+main()
