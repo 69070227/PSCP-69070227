cfb2def9af7aeed1b9e21d7fec6d35b2360176de
69070227 <69070227@kmitl.ac.th>
2026-08-02 23:15:02 +0700

    add

--- oj3031/main.py
+++ oj3031/main.py
@@ -7,9 +7,8 @@ def main():
 
     for _ in range(people): #ทำตามค่าคนไปเรื่อยๆ ของแต่ละคน
         x,y = map(int,input().split())
-        area = 3.1416 * (x**2 + y**2)
-        time = math.ceil(area / speed)
+        area = 3.1416 * (x**2 + y**2) # πr^2 (r^2=x^2+y^2)
+        time = math.ceil(area / speed) #mathceil คือการปัดขึ้น
         print(time)
 
 main()
-not done
