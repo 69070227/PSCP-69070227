a56e4b87b69697f19824366bb00b4824e7adbd22
69070227 <69070227@kmitl.ac.th>
2026-08-12 13:26:28 +0700

    add

--- oj3105_work/main.py
+++ oj3105_work/main.py
@@ -1,1 +1,12 @@
-print()
+"""taxi"""
+
+distance = int(input())
+price = 35
+
+for i in range(2, distance + 1):
+    if i <= 10:
+        price += 5
+    else:
+        price += 8
+
+print(price)
--- oj3116_work/main.py
+++ oj3116_work/main.py
@@ -4,33 +4,28 @@ def main():
     """ASCII คือ รหัสตัวเลขแทนตัวอักษร ดังนั้นใช้ฟังก์ชัน ord() ในการแปลงจากตอศ.เป็นตัวเลข"""
     school_input = input()
 
-    first_letter = school_input[0].upper()
-    last_letter = school_input[-1].upper()
+    first_ASCII = ord(school_input[0].upper())
+    last_ASCII = ord(school_input[-1].upper())
 
     length = len(school_input)
     list_ascii = []
 
 
-    ASCII_first = ord(first_letter)
-    ASCII_last = ord(last_letter)
-
     for i in range(1,11):
         place_value = i - 1
-        if i % 2 != 0 :
-            value = ASCII_first + place_value
-            list_ascii.append(value)
+        if i % 2 :
+            value = first_ASCII + place_value
         else:
-            value = ASCII_last - place_value
-            list_ascii.append(value)
+            value = last_ASCII - place_value
 
-    rem = value % length
-    if rem > 9:
-        rem = rem % 10
+        rem = value % length
+        if rem > 9:
+            rem = rem % 10
 
         list_ascii.append(rem)
 
     middle_6 = list_ascii[2:8]
 
-    print("".join(map(str, middle_6)))
+    print(" ".join(map(str, middle_6)))
 
 main()
