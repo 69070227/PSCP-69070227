e6d51d146c2c98fa7befef799901d8bfbbe915d5
69070227 <69070227@kmitl.ac.th>
2026-08-13 21:18:51 +0700

    add

--- oj3166_work/main.py
+++ oj3166_work/main.py
@@ -5,17 +5,23 @@ subject_N = int(input())
 total = []
 RESULT = ""
 for i in range(subject_N):
-    add_subject = int(input())
+    add_subject = float(input())
     total.append(add_subject)
+
+result_total = sum(total)
+average = result_total /subject_N
+average = math.ceil(average * 100) / 100
 
 RESULT = "PASS"
 for i in total:
     if i <= 50:
-        RESULT = "FAIL"
-        break
+        if average <= 50:
+            RESULT = "FAIL"
+            break
+    else:
+        if average > 60:
+            RESULT = "PASS"
 
-total = sum(total)
-average = total /subject_N
-average = math.ceil(average * 100) / 100
+
 print(f"{average:.1f}")
 print(RESULT)
